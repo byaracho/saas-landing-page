49a8bcbb82f8413f86b6b462a15aa74be7dab4b1
aracho <byaracho@gmail.com>
2024-06-25 12:36:45 +0900

    add: 슬라이드 구현

--- script.js
+++ script.js
@@ -19,3 +19,44 @@ document.documentElement.addEventListener("click", function () {
         toggleDropdown();
     }
 });
+
+let pages = 0;
+let positionValue = 0;
+// const feedbackWidth = document.querySelector(".feedback").clientWidth;
+
+const prevBtn = document.querySelector(".prev")
+const nextBtn = document.querySelector(".next")
+const feedbacks = document.querySelector(".feedbacks")
+
+function next() {
+    if (pages < 2) {
+        prevBtn.removeAttribute('disabled')// 뒤로 이동해 더이상 disabled가 아니여서 속성을 삭제한다.
+        positionValue -= document.querySelector(".feedback").clientWidth;// feedbackWidth의 증감을 positionValue에 저장한다.
+        feedbacks.style.transform = `translateX(${positionValue}px)`;
+        // x축으로 positionValue만큼의 px을 이동한다.
+        pages += 1; // 다음 페이지로 이동해서 pages를 1증가 시킨다.
+    }
+    if (pages === 2) { //
+        nextBtn.setAttribute('disabled', 'true')// 마지막 장일 때 next버튼이 disabled된다.
+    }
+}
+
+function back() {
+    if (pages > 0) {
+        nextBtn.removeAttribute('disabled')
+        positionValue += document.querySelector(".feedback").clientWidth;
+        feedbacks.style.transform = `translateX(${positionValue}px)`;
+        pages -= 1; // 이전 페이지로 이동해서 pages를 1감소 시킨다.
+    }
+    if (pages === 0) {
+        prevBtn.setAttribute('disabled', 'true')// 마지막 장일 때 back버튼이 disabled된다.
+    }
+}
+
+function init() {  // 초기 화면 상태
+    prevBtn.setAttribute('disabled', 'true'); // 속성이 disabled가 된다.
+    prevBtn.addEventListener("click", back); // 클릭시 다음으로 이동한다.
+    nextBtn.addEventListener("click", next);// 클릭시 이전으로 이동한다.
+}
+init();
+
